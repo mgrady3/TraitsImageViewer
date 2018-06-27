619fa9f3abda56160cba3d6b73236f02e4369253
Maxwell Grady <max.grady@gmail.com>
2018-06-27 17:29:53 -0500

    bump version micro

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'TraitsImageViewer',
-    version = '0.1.0',
+    version = '0.1.5',
     url = '',
     author = 'Maxwell Grady',
     author_email = 'mgrady@enthought.com',
