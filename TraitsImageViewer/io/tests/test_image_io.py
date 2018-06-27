45771aa67ff9c70740617d42c7db66a5b56d47fc
Maxwell Grady <max.grady@gmail.com>
2018-06-27 17:01:44 -0500

    Add test for greyscale image

--- TraitsImageViewer/io/tests/test_image_io.py
+++ TraitsImageViewer/io/tests/test_image_io.py
@@ -23,5 +23,21 @@ class TestImageIO(object):
         # then
         assert isinstance(im, TIV.models.ImageModel.ImageModel)
         assert im.color_depth == 3  # RGB image
+        assert im.data.ndim == 3
         assert im.data.shape[2] == 3
+
+    def test_load_image_greyscale_PIL(self):
+         # given
+        image_path = op.realpath(op.join(TIV.__file__,
+                                         os.pardir,
+                                         "test_data",
+                                         "ozark-mountains-bw.jpg"))
+        # when
+        im = imio.load_image_from_file_PIL(image_path)
+
+        # then
+        assert isinstance(im, TIV.models.ImageModel.ImageModel)
+        assert im.color_depth == 1  # Greyscale image
+        assert im.data.ndim == 2
+
 
