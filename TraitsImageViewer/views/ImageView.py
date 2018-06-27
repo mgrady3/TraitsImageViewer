f3741f9f4f4bdab61429b3e1006a020cde75d49c
Maxwell Grady <max.grady@gmail.com>
2018-06-27 18:11:22 -0500

    Move image view testing into /tests/

--- TraitsImageViewer/views/ImageView.py
+++ TraitsImageViewer/views/ImageView.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pyqtgraph as pg
-from PyQt5 import (
-    QtCore, QtGui, QtWidgets,
-)
+from PyQt5 import QtWidgets
 import sys
 
 from TraitsImageViewer.models.ImageModel import ImageModel
@@ -19,7 +17,7 @@ class ImageView(pg.PlotItem):
         # self.setImage(self.model.data)
 
 
-# View Tests
+# View Tests / Utility methods
 
 def gen_test_model_random2D():
     test_data = np.random.randint(0, 2**16 -1, (512, 512)).astype(np.int64)
@@ -29,21 +27,3 @@ def gen_test_model_random2D():
         height=test_data.shape[0],
         width=test_data.shape[1],
     )
-
-def main():
-    app = QtGui.QApplication([])
-
-    # win = QtWidgets.QMainWindow()
-    # win.setWindowTitle("Traits-ImageViewer")
-   
-    view = pg.GraphicsView()
-    im = gen_test_model_random2D()
-    imv = ImageView(parent=view, model=im)
-    view.setCentralWidget(imv)
-    # win.setCentralWidget(imv)
-    # win.show()
-    view.show()
-    sys.exit(app.exec_())
-
-if __name__ == '__main__':
-    main()
