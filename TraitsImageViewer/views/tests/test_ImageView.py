1f76dc37c0a85219e48d34fdb25693d8772ebc21
Maxwell Grady <max.grady@gmail.com>
2018-06-28 16:04:51 -0500

    Add test for actual image BW

--- TraitsImageViewer/views/tests/test_ImageView.py
+++ TraitsImageViewer/views/tests/test_ImageView.py
@@ -7,7 +7,8 @@ import pyqtgraph as pg
 
 from TraitsImageViewer.models.ImageModel import ImageModel
 from TraitsImageViewer.views.ImageView import (
-    ImageView, gen_test_model_random2D
+    ImageView, gen_test_model_random2D,
+    gen_test_model_actual_image_BW
 )
 
 app = QtWidgets.QApplication([])
@@ -27,10 +28,23 @@ class ImageViewTest(unittest.TestCase):
 
         # then
         self.view.show()
+    
+    def test_view_actual_BW(self):
+        # given
+        im = gen_test_model_actual_image_BW()
+
+        # when
+        self.imv = ImageView(parent=self.view, model=im)
+        self.view.setCentralWidget(self.imv)
+
+        # then
+        self.view.show()
+
 
 
 if __name__ == '__main__':
     ivt = ImageViewTest()
     ivt.setUp()
     ivt.test_view_random2D()
+    ivt.test_view_actual_BW()
     sys.exit(app.exec_())
