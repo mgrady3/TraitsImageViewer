cb00d7252b2f679d7ad85cbc25cfcded92f663be
Maxwell Grady <max.grady@gmail.com>
2018-07-14 00:34:34 -0500

    add method to load raw data as 2D numpy array.

--- TraitsImageViewer/io/ImageIO.py
+++ TraitsImageViewer/io/ImageIO.py
@@ -6,11 +6,30 @@ also read and write raw data (.dat files).
 """
 
 import numpy as np
+import os
 from PIL import Image
 from TraitsImageViewer.models.ImageModel import ImageModel
 
 
+class InvalidParameterError(Exception):
+    """Indicate that required params for parsing data files are not present."""
+
+    def __init__(self, message=None):
+        if message is None:
+            message = "Error: Invalid or Insufficient Parameters required to process data files."
+        super(InvalidParameterError, self).__init__(message)
+
+class FormatNotSupportedError(Exception):
+    """ Indicate that image format is not supported."""
+
+    def __init__(self, message=None):
+        if message is None:
+            message = "Error: Requested image format is not yet supported."
+        super(InvalidParameterError, self).__init__(message)
+
+
 def load_image_from_file_PIL(path):
+    """ Generate ImageModel from file using PIL.Image.open()."""
     try:
         im = Image.open(path)
     except:
@@ -34,3 +53,47 @@ def load_image_from_file_PIL(path):
         height=data.shape[0], 
         width=data.shape[1] 
     )
+
+def load_image_from_file_raw_2D(path, ht=None, wd=None, 
+                                bits=None, byte_order=None):
+    """ Load .dat file into 2D numpy array.
+
+    :param path: string path to file
+    :param ht: integer pixel height of image
+    :param wd: integer pixel width of image
+    :param bits: integer representing bit depth of image, i.e. bits per pixel - 
+        default is 16 bit
+    :param byte_order: string representing byte order,
+        'L' for Little-Endian (Intel), 
+        'B' for Big-Endian (Motorola)
+    :return dat_arr: 2D numpy array
+    """
+    # ensure all params are not None
+    if (not ht and wd and bits and byte_order) or not os.path.exists(path):
+        raise InvalidParameterError
+
+    if not bits % 8 == 0:
+        raise InvalidParameterError
+
+    bits_per_byte = 8
+    
+    if bits == 8 and byte_order == 'L':
+        formatstring = '<u1'  # 1 byte (8 bits) per pixel
+    elif bits == 8 and byte_order == 'B':
+        formatstring = '>u1'
+
+    elif bits == 16 and byte_order == 'L':
+        formatstring = '<u2'  # 2 bytes (16 bits) per pixel
+    elif bits == 16 and byte_order == 'B':
+        formatstring = '>u2'
+    elif bits > 16 or byte_order not in ['L', 'B']:
+        raise FormatNotSupportedError
+    
+    # read file, discard header info, convert image data into numpy array
+    with open(path, 'rb') as f:
+        header_length = len(f.read()) - (int(bits/bits_per_byte) * ht * wd)
+        f.seek(0)
+        return np.fromstring(f.read()[header_length:],
+                                formatstring).reshape((ht, wd))
+    
+       
