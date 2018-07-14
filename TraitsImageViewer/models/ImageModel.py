6d83319569654323ffb4a5eec7b9e782f96a0d21
Maxwell Grady <max.grady@gmail.com>
2018-07-14 00:36:25 -0500

    Adjust comments for color_depth

--- TraitsImageViewer/models/ImageModel.py
+++ TraitsImageViewer/models/ImageModel.py
@@ -8,8 +8,8 @@ from traits.api import (
 class ImageModel(HasTraits):
     """ Representation of a 2D image using a numpy 2d array."""
 
-    color_depth = Int  # Greyscale, RGB, RGBA
-    data = ArrayOrNone  # 2d numpy array
+    color_depth = Int  # valid values: 1, 3, 4 i.e. Greyscale, RGB, RGBA
+    data = ArrayOrNone  # 2D numpy array
     height = Int
     width = Int
 
@@ -23,10 +23,10 @@ class ImageModel(HasTraits):
 class ImageStack(HasTraits):
     """ Representation of a 3D stack of 2D images; (HxWxD)."""
 
-    color_depth = Int # Greyscale, RGB, RGBA
+    color_depth = Int # valid values: 1, 3, 4 i.e. Greyscale, RGB, RGBA
     current_image = ImageModel
     current_image_index = Int
-    data = ArrayOrNone  # 3d numpy array
+    data = ArrayOrNone  # 3D numpy array
     depth = Int  # Number of images
     height = Int
     width = Int
@@ -52,16 +52,4 @@ class ImageStack(HasTraits):
 
     def valid_index(self, index):
         return index in range(self.depth)  # [0, depth - 1]
-    
-
-
-    
-
-    
-
-
-
-
-
-
     
